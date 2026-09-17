2f46e2850e1ce3ce2cc5f6b0a4834d7b932c3305
RJ <RJ@gmail.com>
2026-09-17 19:32:37 +0530

    initial app code

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
